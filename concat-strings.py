34b50c806a0868b57debf674593767438c0e0953
ahehir101 <124339153+ahehir101@users.noreply.github.com>
2023-11-28 15:53:28 +0000

    Add files via upload

---
+++ concat-strings.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+color = input("What is your favorite color?  ")
+animal = input("What is your favorite animal?  ")
+print("{}, you like a {} {}!".format(name,color,animal))
---
+++ oldcollections.py
@@ -0,0 +1,5 @@
+myFruitList = ("apple", "banana", "cherry")
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[1])
+myFruitList[2] = "orange"
